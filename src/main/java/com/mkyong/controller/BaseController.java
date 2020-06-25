dfaa3747c3d07745601212ba88e2da968dbce893
rohitchaitanya27 <52485345+rohitchaitanya27@users.noreply.github.com>
2020-06-25 14:21:25 +0530

    Update BaseController.java

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Welcome to the devops. This is the special request");
+		model.addAttribute("message", "Welcome to the devops session Pre sales team");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
